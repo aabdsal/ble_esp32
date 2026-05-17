dd2a617357597e3d522d1e781542906f1a3482cf
Jaume Parra Peris <jparper@etsinf.upv.es>
2026-05-17 13:00:51 +0200

    Uncomment logging and BLE send functionality

--- mando/main/main.c
+++ mando/main/main.c
@@ -45,33 +45,32 @@ void app_main(void)
         {
             servo++;
             if (servo > 6) servo = 1;
-            //ESP_LOGI(tag, "Servo seleccionado: %d", servo);
-            ESP_LOGI(tag, "Boton select seleccionado");
+            ESP_LOGI(tag, "Servo seleccionado: %d", servo);
             vTaskDelay(pdMS_TO_TICKS(300));
         }
 
         if (mando_btn_right_read())
         {
-            //char msg[10];
+            char msg[10];
             ESP_LOGI(tag, "Boton right seleccionado");
-            //ble_send(msg);
+            ble_send(msg);
             vTaskDelay(pdMS_TO_TICKS(300));
         }
         
         if (mando_btn_left_read())
         {
-            //char msg[10];
+            char msg[10];
             ESP_LOGI(tag, "Boton left seleccionado");
-            //ble_send(msg);
+            ble_send(msg);
             vTaskDelay(pdMS_TO_TICKS(300));
         }
 
         
         if (mando_btn_ok_read())
         {
-            //char msg[10];
+            char msg[10];
             ESP_LOGI(tag, "Boton ok seleccionado");
-            //ble_send(msg);
+            ble_send(msg);
             vTaskDelay(pdMS_TO_TICKS(300));
         }
 
